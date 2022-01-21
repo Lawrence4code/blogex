7375a3e1a972eab377ab963a1f7b830b695cbea7
Lawrence <Lawrence4code@gmail.com>
2022-01-22 01:01:31 +0530

    feature: Make a post api call with Gql

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,27 @@
 import Head from 'next/head';
-import { PostCard, Categories, PostWidget } from './../components'
+import { PostCard, Categories, PostWidget } from './../components';
+import { getPosts } from './../services'
 
-const posts = [
-  {
-    id: 1,
-    title: 'React performance',
-    excerpt: 'Learn React performance',
-  },
-  {
-    id: 2,
-    title: 'React with Tailwind',
-    excerpt: 'Learn React with Tailwind',
-  }
-]
+// const posts = [
+//   {
+//     id: 1,
+//     title: 'React performance',
+//     excerpt: 'Learn React performance',
+//   },
+//   {
+//     id: 2,
+//     title: 'React with Tailwind',
+//     excerpt: 'Learn React with Tailwind',
+//   }
+// ]
 
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log('posts  111', posts)
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title> Blogex</title>
+        <title>Blogex</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -40,4 +42,11 @@ export default function Home() {
 
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts() || []);
+  return {
+    props: { posts }
+  }
 }
