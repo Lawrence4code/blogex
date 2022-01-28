20372c9e30dcb81680c268a7b071e14996b69e81
Lawrence <Lawrence4code@gmail.com>
2022-01-28 10:10:33 +0530

    Add featured post functionality

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { PostCard, Categories, PostWidget } from './../components';
-import { getPosts } from './../services'
+import { getPosts } from './../services';
+import { FeaturedPosts } from '../sections'
 
 // const posts = [
 //   {
@@ -23,6 +24,7 @@ export default function Home({ posts }) {
         <title>Blogex</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className='lg:col-span-8 col-span-1'>
           {
